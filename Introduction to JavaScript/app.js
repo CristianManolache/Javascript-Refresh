c74c19325b8652e7738366ceb1c726722b3aa2e1
CristianManolache <mlcristian@outlook.com>
2022-12-05 11:27:34 +0000

    Datatypes and Variables
    Working with Datatypes and Variables , quick introduction to arrays

--- Introduction to JavaScript/app.js
+++ Introduction to JavaScript/app.js
@@ -1,4 +1,18 @@
 //Tell the computer to remember something
+//Declaring and Assigning
 var name = "Laurentiu"; //DataType String
 var id = 29011996; //DataType Number
 var alive = true; //DataType Boolean
+var abc; //result undefined 
+
+
+//Overwriting Assigning new values
+name = "Sharon";
+id = 2005;
+alive = false;
+abc = null; // means nothing
+
+console.log(id);
+console.log(name);
+console.log(alive);
+console.log(abc);
---
+++ Working with Javascript Variables and Datatypes/app.js
@@ -0,0 +1,36 @@
+//Tell the computer to remember something
+//Declaring and Assigning
+var name = "Laurentiu"; //DataType String
+var id = 29011996; //DataType Number
+var alive = true; //DataType Boolean
+var abc; //result undefined 
+//This are simple DataType
+
+name = "Cristian";
+name = name + " Laurentiu";
+
+console.log(name);
+
+id = 25 + 2;
+console.log(id);
+
+id = 5 * 6;
+console.log(id);
+
+id = id / 4;
+console.log(id);
+
+id = id - 12;
+console.log(id);
+
+//Complex DataType
+
+abc = ["Dodge", "Duck", "Dip", "Dodge"];
+//Array = (list) , Array is a complex or compound data type 
+console.log(abc);
+console.log(abc.length); //length
+
+console.log(abc[0]); // [] = index ( item 0 , 1 , 2 , 3 ,4 )
+console.log(abc[0] + abc[1] + abc[2]); //concatenate without space
+console.log(abc[0] + " " + abc[1] + " " + abc[2]); //concatenated with space
+
