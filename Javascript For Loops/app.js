4384b5fc56a40f1c4c651de8f2f856609de4eaad
CristianManolache <mlcristian@outlook.com>
2022-12-05 22:39:54 +0000

    For Loops Basic

---
+++ Javascript For Loops/app.js
@@ -0,0 +1,8 @@
+//Javascript for loops
+var names = ['Lazer', 'Bilutsa1', 'avocatul', 'dracula', 'SimPaticul', 'Norocosul', 'karl'];
+var totalNames = names.length;
+
+//initialize; test; increment
+for (let i = 0; i < totalNames; i++) {
+    console.log(names[i]);
+} 
