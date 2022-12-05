b005cafb42cc317f66731049ae2812a487bc5eee
CristianManolache <mlcristian@outlook.com>
2022-12-05 16:53:46 +0000

    Javascript Ternary Operator
    How to use a ternary statement , with one simple question of if statement.

---
+++ Javascript Ternary Operator/app.js
@@ -0,0 +1,20 @@
+var first_name = "Alex";
+var last_name = "Bobby";
+var age = 26;
+var first = true;
+var last = false;
+var bob;
+
+if (first == last) {
+    console.log("This can never happen.");
+
+} else {
+    console.log("You're only last if you're not first.");
+}
+
+//Ternary statment simple if statment with one simple question can you use the ternary statment
+// ()? : ;
+
+first == last ? console.log("nope") : console.log("yep");
+
+first == last ? console.log("no it's not true") : (age) ? console.log("Yes he is 26 'true' ") : console.log("not available");
