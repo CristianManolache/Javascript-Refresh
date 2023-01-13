ed9cf98fa81a6fe979c62ff52964eec56c5c0754
CristianManolache <mlcristian@outlook.com>
2023-01-13 22:09:01 +0000

    Update app.js

--- Javascript While and Do Loops/app.js
+++ Javascript While and Do Loops/app.js
@@ -13,8 +13,20 @@ do{
 let number = 0;
 
 while (number < 10) {
-    number++
+    number++;
     console.log(number);
 }
 
 let numeri = 0;
+
+do {
+    numeri++;
+    console.log(numeri);
+} while (numeri < 10);
+
+
+let names = ["Sharon", "Laur", "Gianina", "Teo"];
+
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
