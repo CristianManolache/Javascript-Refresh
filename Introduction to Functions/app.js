e2fefa8e01e6e80825e7609354057719ce112db8
CristianManolache <mlcristian@outlook.com>
2023-01-14 20:38:25 +0000

    Drink beer function user

---
+++ Introduction to Functions/app.js
@@ -0,0 +1,13 @@
+//food prices
+var beer_cost = 6.75;
+var burger_cost = 5.00;
+var pop_cost = 3.00;
+
+//user's money
+var account_balance = 15.75
+
+//drink beer function
+
+account_balance = account_balance - beer_cost;
+console.log("You spend " + beer_cost, "at Lazy Bar", "Account balance is: " + account_balance);
+
