1ce81cf8ab0850fce8c6aa09388c6a029847830f
CristianManolache <mlcristian@outlook.com>
2023-01-14 21:52:01 +0000

    Update app.js
    Basic function to hold

--- Introduction to Functions/app.js
+++ Introduction to Functions/app.js
@@ -7,7 +7,29 @@ var pop_cost = 3.00;
 var account_balance = 15.75
 
 //drink beer function
+function drink_beer() {
+    account_balance = account_balance - beer_cost;
+    console.log("You spend " + beer_cost, "at Lazy Bar", ",", "Account balance now is: " + account_balance);
+}
 
-account_balance = account_balance - beer_cost;
-console.log("You spend " + beer_cost, "at Lazy Bar", "Account balance is: " + account_balance);
+//burger eat function
+function eat_burger() {
+    account_balance = account_balance - burger_cost;
+    console.log("You spend " + burger_cost, "at Lazy Bar", ",", "Account balance now is: " + account_balance);
+}
 
+//pop drink function
+function drink_pop() {
+    account_balance = account_balance - pop_cost;
+    console.log("You spend " + pop_cost, "at Lazy Bar", ",", "Account balance now is: " + account_balance);
+}
+
+//remaining
+function acc_balancetodate() {
+    console.log("Account balance now is: " + account_balance);
+}
+
+acc_balancetodate();
+drink_beer();
+eat_burger();
+acc_balancetodate();
