a1a13befab6e23d137ee84e5f22871878997d37e
CristianManolache <mlcristian@outlook.com>
2023-01-17 21:42:42 +0000

    Updated  check balance
    Trying to solve the error app.js:57 Uncaught TypeError: mList[i] is not a function
        at visitChristmas (app.js:57:17)
        at app.js:62:1

--- Introduction to Functions/app.js
+++ Introduction to Functions/app.js
@@ -3,42 +3,53 @@ var beer_cost = 6.75;
 var burger_cost = 5.00;
 var pop_cost = 3.00;
 var bank_ballance = 100;
-var bank_request = 0;
 
 //user's cash money
-var account_balance = 15.75
+var account_balance = 25.75
+//check account balance
+function checkBalance(amt) {
+    if (account_balance - amt >= 0) {
+        return true;
+    } else {
+        console.log("INSUFFICIENT FUNDS");
+        return false;
+    }
+}
 
 //drink beer function
 function drink_beer() {
-    account_balance = account_balance - beer_cost;
-    console.log("You spend " + beer_cost, "at Lazy Bar", ",", "Account balance now is: " + account_balance);
+    if (checkBalance(beer_cost)) {
+        account_balance = account_balance - beer_cost
+        console.log("Enjoy your beer");
+    }
+    console.log("You spend " + beer_cost, "at Lazy Bar", ",", "Account balance now is: " + account_balance.toFixed(2));
 }
 
 //burger eat function
 function eat_burger() {
-    account_balance = account_balance - burger_cost;
-    console.log("You spend " + burger_cost, "at Lazy Bar", ",", "Account balance now is: " + account_balance);
+    if (checkBalance(burger_cost)) {
+        account_balance = account_balance - burger_cost;
+        console.log("Enjoy your burger");
+    }
+    console.log("You spend " + burger_cost, "at Lazy Bar", ",", "Account balance now is: " + account_balance.toFixed(2));
 }
 
 //pop drink function
 function drink_pop() {
-    account_balance = account_balance - pop_cost;
-    console.log("You spend " + pop_cost, "at Lazy Bar", ",", "Account balance now is: " + account_balance);
+    if (checkBalance(pop_cost)) {
+        account_balance = account_balance - pop_cost;
+        console.log("Enjoy your pop");
+    }
+    console.log("You spend " + pop_cost, "at Lazy Bar", ",", "Account balance now is: " + account_balance.toFixed(2));
 }
 
 //remaining
 function acc_balancetodate() {
     console.log("Account balance now is: " + account_balance);
 }
-
-//bank withdraw
-function bank_withdraw() {
-    bank_ballance
-}
-
 //visit the Christmas Market --> eat and drink
 
-var mealList = [eat_burger, drink_beer, drink_pop, drink_beer, acc_balancetodate];
+var mealList = [eat_burger, drink_beer, drink_pop, drink_beer];
 
 function visitChristmas(mList) {
     //mList is a parameter with no value inside
@@ -47,4 +58,9 @@ function visitChristmas(mList) {
     }
 }
 //mList = mealList after we call the function bellow.
-visitChristmas(mealList);
+
+visitChristmas([eat_burger, eat_burger, drink_beer]);
+visitChristmas([eat_burger, eat_burger, drink_beer, drink_pop]);
+
+
+
